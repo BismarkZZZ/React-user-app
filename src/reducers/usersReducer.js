773bc3ab33162e3dba783f0778727a2723d0b2d4
Tardieh <tardiehbismark@hotmail.com>
2021-03-20 01:01:58 -0400

    Add case for deleting user

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -13,7 +13,9 @@ const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_USER":
             return {...state, users: [...state.users, action.payload]};
-        
+        case "DELETE_USER":
+            let undeletedUsers = state.users.filter((user) => user.id !== action.payload);
+            return {...state, users: undeletedUsers}
         default:
             return state;
     }
