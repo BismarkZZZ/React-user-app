0acd65b2649bebf1d7e124d1255b07af72bb003f
Tardieh <tardiehbismark@hotmail.com>
2021-03-19 22:56:59 -0400

    Create action for New User

---
+++ src/actions/usersAction.js
@@ -0,0 +1,9 @@
+//action creator for adding user
+export const addUser = (newUser) => {
+    newUser.id = Math.random().toString();
+    return {
+        type: "ADD_USER",
+        payload: newUser
+    }
+}
+
