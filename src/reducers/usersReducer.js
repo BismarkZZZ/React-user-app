21246269146920f661d218c9011dcf468da5649a
Tardieh <tardiehbismark@hotmail.com>
2021-03-19 23:15:51 -0400

    Connect usersReducer to State

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -12,7 +12,7 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_USER":
-            return state;
+            return {...state, users: [...state.users, action.payload]};
         
         default:
             return state;
