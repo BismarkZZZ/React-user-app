fee4d258a8c408b32374b2dbc808226157f9e886
Tardieh <tardiehbismark@hotmail.com>
2021-03-20 01:34:00 -0400

    Add  Edit user case

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -2,7 +2,7 @@ const initialState = {
     users: [
         {
             name: "Meeky",
-            email: "Meekyx@oasis..com",
+            email: "Meekyx@oasis.com",
             gen: 9,
             id: "8932bh2gv8293b",
         },
@@ -16,6 +16,9 @@ const userReducer = (state = initialState, action) => {
         case "DELETE_USER":
             let undeletedUsers = state.users.filter((user) => user.id !== action.payload);
             return {...state, users: undeletedUsers}
+        case "EDIT_USER":
+            return {...state, users: state.users.map((user) =>
+                user.id === action.payload.id ? action.payload : user),}
         default:
             return state;
     }
